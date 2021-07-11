a0fb95292d851a98a75a3b4ca23ed841cd2b02b7
Tetsuo Honda <t-honda@hondarer-soft.com>
2021-07-12 07:55:09 +0900

    initial commit

---
+++ WPFDateTimePicker/WPFDateTimePicker/ViewModels/DateTimePickerViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDateTimePicker.ViewModels
+{
+    public class DateTimePickerViewModel : BindableBase
+    {
+    }
+}
